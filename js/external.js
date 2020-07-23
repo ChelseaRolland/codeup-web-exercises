de38bad0be6e4df8b523679071f9f901e9c9b435
Chelsea Rolland <cerapture@Chelseas-MacBook-Pro.local>
2020-07-23 18:39:35 -0500

    On the last question four

--- js/external.js
+++ js/external.js
@@ -11,15 +11,23 @@ console.log("message", "Responding to the answer");
 
 //Exercise 3: Question 1
 alert("You will be renting movies for your children. Please let us know how many days you will be keeping each movie.")
+
+//Making the variable string output into numbers
 var mermaid = prompt("How many days will you be renting the movie: Little Mermaid?");
+console.log(typeof mermaid);
+
 var mermaidNum = Number(mermaid);
 console.log(typeof mermaidNum);
 
 var bear = prompt("How many days will you be renting the movie: Brother Bear");
+console.log(typeof bear);
+
 var bearNum = Number(bear);
 console.log(typeof bearNum);
 
 var herc = prompt("How many days will you be renting the movie: Hercules?");
+console.log(typeof herc);
+
 var hercNum = Number(herc);
 console.log(typeof hercNum);
 
@@ -29,6 +37,7 @@ console.log(typeof subtotal);
 var pricePerDayConfirmed = confirm("Each movie cost $3 per day. Do you wish to continue?");
 console.log("note", "Asking if they want to pay $3/day for these movies.");
 
+//Determining how much it will cost to rent the listed above movies
 var total = subtotal * 3;
 console.log(typeof total);
 
@@ -44,26 +53,48 @@ var facebookRate = 350;
 var weekPayMessage = confirm("Please confirm if the associated rates are correct for each company: Google: $400/hr, Amazon: $380/hr, Facebook: $350/hr.");
 console.log("note","Just confirming if the hourly wages are correct");
 
+//Making the variable string output into numbers
 var googleHrs = prompt("How many hours did you work at Google this week?");
+console.log(typeof googleHrs);
+
 var googleHrsNum = Number(googleHrs);
 console.log(typeof googleHrsNum);
 
 var amazonHrs = prompt("How many hours did you work at Amazon this week?");
+console.log(typeof amazonHrs);
+
 var amazonHrsNum = Number(amazonHrs);
 console.log(typeof amazonHrsNum);
 
 var facebookHrs = prompt("How many hours did you work at Facebook this week?");
+console.log(typeof facebookHrs);
+
 var facebookHrsNum = Number(facebookHrs);
 console.log(typeof facebookHrsNum);
 
-
+//Determining how much this person will make this week from the given inputted hours
 var weeklyPay = (googleHrsNum*googleRate) + (amazonHrsNum*amazonRate) + (facebookHrsNum*facebookRate);
 alert("Congrats! You have been paid $" + weeklyPay + " this week!");
 
 //Exercise 3: Question 3
-var classNumberStudents = prompt("How many people are in your class?");
-console.log("message:", "How many people are in the class?")
-var classFull = classNumberStudents <= 30;
+alert("Welcome to your 1st day of class. We hope you enjoy yourself this semester!");
+var students = prompt("How many people are in your class?");
+console.log(typeof students);
 
+var studentsNum = Number(students);
+console.log("message:", "How many people are in the class?")
+var classFull = studentsNum <= 30;
+
+var studentSchedule = confirm("Is your scheduled \"Mon, Wed, and Fri?");
+console.log(studentSchedule);
+console.log(typeof studentSchedule);
+
+//Stating the conflict of the schedule
+var studentConflict = !studentSchedule;
+
+//Determining if the status of the students requirements
+var studentClassChoice = (classFull && (studentSchedule || studentConflict))
+
+alert("Will this student be able to attend this class: " + studentClassChoice + ".");
 
 //Exercise 4: Question 4
