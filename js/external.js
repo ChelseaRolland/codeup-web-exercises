c79af38d1accfa6eb4a5a816d1112bfd78bea4f8
Chelsea <chelsea.e.rolland@gmail.com>
2020-07-24 09:43:38 -0500

    additional cleanup

--- js/external.js
+++ js/external.js
@@ -16,19 +16,16 @@ alert("You will be renting movies for your children. Please let us know how many
 //Making the variable string output into numbers
 var mermaid = prompt("How many days will you be renting the movie: Little Mermaid?");
 console.log(typeof mermaid);
-
 var mermaidNum = Number(mermaid);
 console.log(typeof mermaidNum);
 
 var bear = prompt("How many days will you be renting the movie: Brother Bear");
 console.log(typeof bear);
-
 var bearNum = Number(bear);
 console.log(typeof bearNum);
 
 var herc = prompt("How many days will you be renting the movie: Hercules?");
 console.log(typeof herc);
-
 var hercNum = Number(herc);
 console.log(typeof hercNum);
 
@@ -36,7 +33,7 @@ var subtotal = mermaidNum + bearNum + hercNum;
 console.log(typeof subtotal);
 
 var pricePerDayConfirmed = confirm("Each movie cost $3 per day. Do you wish to continue?");
-console.log("note", "Asking if they want to pay $3/day for these movies.");
+console.log("note:", "Asking if they want to pay $3/day for these movies.");
 
 //Determining how much it will cost to rent the listed above movies
 var total = subtotal * 3;
@@ -44,38 +41,38 @@ console.log(typeof total);
 
 alert("Thank you for your selection. Your total for these movies will be $" + total + ".");
 
+
 //Exercise 3: Question 2
 alert("Congrats! You have done excellent work for the big three: Google, Amazon, and Facebook! Now we need your help in tracking the amount of hours your have worked! So you can get PAID!!!");
 
-var googleRate = 400;
-var amazonRate = 380;
-var facebookRate = 350;
-
-var weekPayMessage = confirm("Please confirm if the associated rates are correct for each company: Google: $400/hr, Amazon: $380/hr, Facebook: $350/hr.");
-console.log("note","Just confirming if the hourly wages are correct");
-
 //Making the variable string output into numbers
+var googleRate = prompt("What is the hourly rate for Google?");
+var googleRateNum = Number(googleRate);
 var googleHrs = prompt("How many hours did you work at Google this week?");
 console.log(typeof googleHrs);
-
 var googleHrsNum = Number(googleHrs);
 console.log(typeof googleHrsNum);
 
+
+var amazonRate = prompt("What is the hourly rate for Amazon?");
+var amazonRateNum = Number(amazonRate);
 var amazonHrs = prompt("How many hours did you work at Amazon this week?");
 console.log(typeof amazonHrs);
-
 var amazonHrsNum = Number(amazonHrs);
 console.log(typeof amazonHrsNum);
 
+var facebookRate = prompt("What is the hourly rate for Facebook?");
+var facebookRateNum = Number(facebookRate);
 var facebookHrs = prompt("How many hours did you work at Facebook this week?");
 console.log(typeof facebookHrs);
-
 var facebookHrsNum = Number(facebookHrs);
 console.log(typeof facebookHrsNum);
 
+
 //Determining how much this person will make this week from the given inputted hours
-var weeklyPay = (googleHrsNum*googleRate) + (amazonHrsNum*amazonRate) + (facebookHrsNum*facebookRate);
+var weeklyPay = (googleHrsNum*googleRateNum) + (amazonHrsNum*amazonRateNum) + (facebookHrsNum*facebookRateNum);
 alert("Congrats! You have been paid $" + weeklyPay + " this week!");
+
 
 //Exercise 3: Question 3
 // enrolled = class not full && class scheduled does not conflict with current schedule
@@ -96,6 +93,7 @@ console.log(typeof studentSchedule);
 var studentClassChoice = classFull && studentSchedule;
 
 alert("Will this student be able to attend this class: " + studentClassChoice + ".");
+
 
 //Exercise 4: Question 4
 // product offer = (person buys more than 2 items and offer has not expired) || (premium members and offer not expired)
