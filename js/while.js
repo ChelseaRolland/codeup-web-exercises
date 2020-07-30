45d7f9ab33420f0c2384d04dd98f3470493b82d7
Chelsea <chelsea.e.rolland@gmail.com>
2020-07-30 17:04:02 -0500

    completed all of the loops exercises

--- js/while.js
+++ js/while.js
@@ -12,53 +12,23 @@ while (x <= 65536) {
 
 // This is how you get a random number between 50 and 100
 var allCones = Math.floor(Math.random() * 50) + 50;
+
+//This is how you get a random number between 10 and 20;
+//var allCones = Math.floor(Math.random() * 10) + 10;
+
 //console.log(allCones);
 
 do {
     var conesBought = Math.floor(Math.random() * 5) + 1;
-    //console.log(conesBought);
-
-    //var conesRemaining = allCones - conesBought
-    //console.log(conesRemaining);
 
     if (allCones > conesBought) {
         console.log(conesBought + " cones sold...");
-    } else if (allCones < 0) {
-        console.log("I can't sell you " + conesBought + ". I only have " + allCones + " left to sell.");
-    } else if (allCones === 0) {
-        console.log("Yay! I have sold them all!")
+    } else if (allCones < conesBought) {
+        console.log("I can't sell you " + conesBought + ". I only have " + allCones + " left to sell.")
+    } else {
+        console.log("Yay! I have sold them all!");
     }
-    // } else {
-    //     console.log("I can't sell you " + conesBought + ". I only have " + allCones + " left to sell.");    }
-    //console.log("I can't sell you " + conesBought + ". I only have " + allCones + " left to sell.");
-    //console.log("I can't sell you " + conesBought + ". I only have " + allCones + " left to sell.");
 
-    // if(allCones > conesBought) {
-    //     console.log(conesBought + " cones sold");
-    // } else if(allCones === 0) {
-    //     console.log("Yay! I have sold them all!")
-    // }
-    // //else if (allCones < conesBought) {
-    //     console.log(("I can't sell you " + conesBought + ". I only have " + allCones + " left to sell."));
-    // }
+    allCones -= conesBought;
 
-    // allcones -= conesBought;
-    // if (allCones > 0) {
-    //     console.log("We still have " + allCones + " left to sell!")
-    // } else if (allCones === 0) {
-    //     console.log("We are completely out of cones!")
-    // } else {
-    //     console.log("I do not have anymore cones")
-    // }
-} while (allCones > 0);
-//while (allCones >= 0)
-
-// This expression will generate a random number between 1 and 5
-// var conesBought = Math.floor(Math.random() * 5) + 1;
-// if ((allCones) && (conesBought === 5)) {
-//     console.log(conesBought + " cones sold");
-// } else if (conesBought < 0) {
-//     console.log("I only have " + conesBought + " cones left to sell")
-// }
-//
-// } while(allCones <= 100);
+} while (allCones >= 0);
