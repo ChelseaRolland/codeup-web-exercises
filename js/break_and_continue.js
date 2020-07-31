71d87d1c1b118627938d6a54f5b5d391595dc388
Chelsea <chelsea.e.rolland@gmail.com>
2020-07-31 10:25:32 -0500

    revised and completed loops

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -12,18 +12,11 @@ while (true) {
         console.log("number input valid");
         break;
     } console.log("number input is not valid");
-
-
-    // if (( typeof userInput === "string") && (!isNaN(userInput))) {
-    //     console.log('number is valid');
-    // } else  {
-    // console.log('number is not valid') };
-    // break;
 }
 
 console.log("Number to skip is: " + userInput);
 
-for (let i = 0; i <= 50; i++) {
+for (let i = 0; i < 50; i++) {
     if (i % 2 === 0) {
         continue;
     }
@@ -34,3 +27,33 @@ for (let i = 0; i <= 50; i++) {
     }
 }
 //}
+
+//with the instructor
+
+var num;
+
+while (true) {
+    num = parseFloat(prompt("Please put an ODD number between 1 and 50"));
+    if (num % 2 !== 0 && num > 0 && num < 51) {
+        break;
+    } //else continue; //do not need this portion; this can be left blank
+}
+
+for (let i = 1; i < 50; i+=2) {
+    if (num === i) {
+        console.log("Yikes! Skipping number: " + i) //or num since num and i are the same)
+        continue;
+    }
+    console.log("Here is an ODD number: " + i);
+}
+
+//while loop below that is the same as a for loop
+var i = 1;
+while(i < 50) {
+    if (i === num) {
+        console.log("Yikes! Skipping the number: " + i ) //or num since num and i are the same)
+        continue;
+    }
+    console.log("Here is an ODD number: " + i);
+    i+=2;
+}
--- js/while.js
+++ js/while.js
@@ -1,12 +1,27 @@
 "use strict";
 
 //While loop
-var x = 2;
+var i = 2;
 
-while (x <= 65536) {
-    console.log(x);
-    x *= 2;
+while (i <= 65536) {
+    console.log(i);
+    i *= 2;
 }
+//
+// //Another way of doing the while loop; counting the iterations
+// var k = 2;
+// var l = 0;
+// while  (l <= 16) {
+//     console.log(Math.pow(2), l);
+//     l++;
+// }
+//
+// //same as the while loop above just refactored as a for loop
+// for (i = 2; i <= 65536; i*=2) {
+//     console.log(i);
+// }
+//
+// //try to refactor the for loops into while loops and vice versa
 
 //DoWhile Loop
 
@@ -18,17 +33,45 @@ var allCones = Math.floor(Math.random() * 50) + 50;
 
 //console.log(allCones);
 
+var conesBought = Math.floor(Math.random() * 5) + 1;
+
 do {
     var conesBought = Math.floor(Math.random() * 5) + 1;
 
-    if (allCones > conesBought) {
+    if (allCones >= conesBought) {
         console.log(conesBought + " cones sold...");
-    } else if (allCones < conesBought) {
-        console.log("I can't sell you " + conesBought + ". I only have " + allCones + " left to sell.")
+        allCones -=conesBought
     } else {
-        console.log("Yay! I have sold them all!");
+        console.log("I can't sell you " + conesBought + ". I only have " + allCones + " left to sell.")
     }
 
-    allCones -= conesBought;
+} while (allCones > 0);
 
-} while (allCones >= 0);
+console.log("Yay! I have sold them all!");
+
+// for (allCones; allCones >= 0; allCones-=conesBought) {
+//     if (allCones > conesBought) {
+//         console.log(conesBought + " cones sold...");
+//     } else if (allCones < conesBought) {
+//         console.log("I can't sell you " + conesBought + ". I only have " + allCones + " left to sell.")
+//     } else {
+//         console.log("Yay! I have sold them all!");
+//     }
+// }
+
+//instructors recap
+// do {
+//     var cones = Math.floor(Math.random() * 5) + 1;
+//     //decide if we have enough
+//     if (cones <= allCones ) {
+//         //sell some cones
+//         var cones = Math.floor(Math.random() * 5) + 1;
+//         console.log(cones + " cones sold...");
+//         allCones -= cones;
+//     } else {
+//         console.log("Cannot sell you " + cones + " cones. I only have " + allCones + " left ...");
+//     }
+//
+//
+// } while (allCones > 0);
+// console.log("Yay! I sold them all")
