312e3bfc8d5f7a78eabd367e0320e5f1f9259fcc
Chelsea <chelsea.e.rolland@gmail.com>
2020-07-31 14:57:51 -0500

    recap for the iterating exercises

--- js/iterating.js
+++ js/iterating.js
@@ -1,4 +1,4 @@
-(function(){
+(function() {
     "use strict";
 
     /**
@@ -90,15 +90,27 @@
      *  > last([1, 2, 3, 4, 5]) // return 5
      */
 
-    function first (arrays) {
+    function first(arrays) {
         console.log(arrays[0]);
     }
+
+    //const first = function(arr) {
+       // return arr[0]
+    //}
+
+    //console.log(first([1,2,3,4,5]));
 
     first([1,2,3,4,5]);
 
     function second (arrays) {
         console.log(arrays[1]);
     }
+
+    //const second = function(arr) {
+    // return arr[1]
+    //}
+
+    //console.log(second([1,2,3,4,5]));
 
     second([1,2,3,4,5]);
 
@@ -108,6 +120,11 @@
          console.log(lastElement);
     }
 
+    //const last = function(arr) {
+    // return arr[arr.length - 1];
+    //}
+
     last([1,2,3,4,5]);
+    //console.log(last(names));
 
 })();
