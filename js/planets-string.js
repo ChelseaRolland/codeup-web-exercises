9cdb72a1310d4110cd3bd0a170a9c42fa5bfc907
Chelsea <chelsea.e.rolland@gmail.com>
2020-07-30 21:38:05 -0500

    still working late tonight, this is it on 7-30-2020

--- js/planets-string.js
+++ js/planets-string.js
@@ -11,7 +11,9 @@
      * console.log planetsArray to check your work
      */
 
-    // console.log(planetsArray);
+    planetsArray = planetsString.split('|');
+
+    console.log(planetsArray);
 
     /**
      * TODO:
@@ -23,5 +25,39 @@
      * list. You will need an opening AND closing <ul> tags around the entire
      * string, and <li> tags around each planet.
      */
+
+    // var planetBreakString = planetsArray.join('<br>');
+    //
+    // console.log(planetBreakString);
+    //
+    // //Bonus
+    //
+    // // for (let i = 0; i < planetsArray.length; i++) {
+    // //     for (let j = 0; j < planetsArray.length; j++) {
+    // //         var planetListString = planetsArray.join('<li>');
+    // //         console.log(planetListString);
+    // //     }
+    // // }
+    //
+    // var planetListString = planetsArray.join('<li>');
+    // console.log(planetListString);
+    //
+    // var planetListArray = planetListString.split();
+    // console.log(planetListArray);
+    //
+    // var planetUnorderdEnd = planetListArray.push('</ul>');
+    // console.log(planetUnorderdEnd);
+    // console.log(planetListArray);
+    //
+    // var planetBeginning = planetListArray.unshift('<ul>');
+    // console.log(planetListArray);
+    //
+    // var planetFinal = planetListArray.join(', ');
+    // console.log(planetFinal);
+
+
+    //var planetUnorderedFirst = planetUnorderedEnd.unshift("<ul>");
+
+    //console.log(planetUnorderedFirst);
 
 })();
