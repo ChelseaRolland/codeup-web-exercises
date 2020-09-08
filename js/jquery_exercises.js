a52f55196e055cc0b27f8be759805630e96d5056
Chelsea <chelsea.e.rolland@gmail.com>
2020-09-08 11:15:35 -0500

    completed the id selectors

---
+++ js/jquery_exercises.js
@@ -0,0 +1,31 @@
+"use strict";
+
+// $(document).ready(function(){
+//     alert("DOM has finished loading!");
+// });
+
+// var contents = $('#codeup').html();
+// alert(contents);
+//
+// $('.important').css('background-color', 'yellow');
+//
+// $('p').css('font-size', '14px');
+//
+// $('.important, p').css('background-color', '#FF0');
+//
+// $('*').css('border', '1px solid #F00');
+
+
+// ID Selectors
+
+// q.3
+var contents = $('#para').html();
+alert(contents);
+
+// q.4
+var contents2 = $('#item-3').html();
+alert(contents2);
+
+//q.5 --> It selected the 1st occurrence
+var contents3 = $('#item-1').html();
+alert(contents3);
