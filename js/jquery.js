c99ec533ab598e76c0ffce8a92a93febf3d54972
Chelsea <chelsea.e.rolland@gmail.com>
2020-09-10 14:21:41 -0500

    still working on the konami code exercise

---
+++ js/jquery.js
@@ -0,0 +1,51 @@
+"use strict";
+
+$(document).ready(function(){
+    console.log('Ready to go!')
+});
+
+var konamiCode = [38,38,40,40,37,39,37,39,66,65,13];
+
+
+// // var konamiCode = ['ArrowUp', 'ArrowUp','ArrowDown','ArrowDown','ArrowLeft','ArrowRight','ArrowLeft','ArrowRight',66,65,13];
+//
+// $('#test-field').on('keyup', function (event) {
+//     console.log('key: ' + event.key);
+//     console.log('key-code: ' + event.keyCode);
+//     // console.log(typeof event.key);
+//     // console.log(typeof event.keyCode);
+//     // console.log(typeof konamiCode[0]);
+//     // var konamiCode = [38,38,40,40,37,39,37,39,66,65,13];
+//
+//     let finalArray = [];
+//
+//     for (let i = 0; i < konamiCode.length; i++) {
+//         // let finalArray = [];
+//         if (konamiCode[i] === event.keyCode) {
+//           finalArray.push(event.keyCode)
+//         } // else if (konamiCode[i] === event.key) {
+//         //     finalArray.push(event.key);
+//         // }
+//         else {
+//             console.log('This is not the correct code!');
+//         }
+//     } return finalArray;
+// })
+
+// function checkCode() {
+//     let finalArray = [];
+//     konamiCode.forEach(function(code){
+//         if (code === event.keyCode) {
+//             finalArray.push(code);
+//         } else {
+//             console.log('This is not the correct code!');
+//         }
+//     });
+//     return finalArray;
+
+
+
+
+// console.log(checkCode());
+//
+// $('#test-field').on('keyup', checkCode);
