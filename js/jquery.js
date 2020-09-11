ae7cad2e71b2a2255d83bb544eab1e2eccf5d7ac
Chelsea <chelsea.e.rolland@gmail.com>
2020-09-11 09:48:42 -0500

    still working on the redo of the konami code

--- js/jquery.js
+++ js/jquery.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
         //Need to create a code for the input of the correct Konami Codes
 
         //List out the variables
-        let konamiCode = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65, 13];
+        let konamiCode = [38,38,40,40,37,39,37,39,66,65,13];
         let count = 0;
         let userInput = event.keyCode;
         let start = false;
@@ -18,14 +18,39 @@ $(document).ready(function() {
         if (!start) {
             if (userInput === konamiCode[count]) {
                 start = true;
+                // count++;
+                console.log("Lets go!")
             }
         }
 
-        //Once the input has started and is correct
+        //Once the input has started and  //Comparing the Userinput to the Actual Konami Code
+        if (start) {
 
-        //Comparing the Userinput to the Actual Konami Code
+            //Comparing the userinput to the konami code
+            if (userInput === konamiCode[count]){
+                console.log('You doing right!');
+                count++;
+            } else {
+                reset();
+            }
+            //Tell the user they got the correct code
+            if (count === konamiCode.length) {
+                console.log("You did it!")
+                youWon();
+            }
 
+        } else {
+            reset();
+        }
 
+        function reset() {
+            start = false;
+            count = 0;
+        }
+
+        function youWon() {
+            $('body').css('background-color', 'grey');
+        }
 
     });
 
