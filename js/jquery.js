9e6d500eae8a90298062667e3e3a091800a12423
Chelsea <chelsea.e.rolland@gmail.com>
2020-09-11 09:02:35 -0500

    start with the game starting

--- js/jquery.js
+++ js/jquery.js
@@ -11,9 +11,21 @@ $(document).ready(function() {
         //List out the variables
         let konamiCode = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65, 13];
         let count = 0;
-        let neededKeys = event.keyCode;
+        let userInput = event.keyCode;
+        let start = false;
 
-//
+        //If the code input has started
+        if (!start) {
+            if (userInput === konamiCode[count]) {
+                start = true;
+            }
+        }
+
+        //Once the input has started and is correct
+
+        //Comparing the Userinput to the Actual Konami Code
+
+
 
     });
 
