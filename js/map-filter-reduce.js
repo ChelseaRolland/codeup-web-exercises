5167cfa4eaf77ec50d73d1c65d5aaf816d273996
Chelsea <chelsea.e.rolland@gmail.com>
2020-09-18 14:41:26 -0500

    completed the map-filter-reduce exercise

--- js/map-filter-reduce.js
+++ js/map-filter-reduce.js
@@ -52,20 +52,47 @@
     //q.4
     // TODO: Use .reduce to get the total years of experience from the list of users. Once you get the total of years you can use the result to calculate the average.
         let totalExperience = users.reduce( (years, user) => {return years + user.yearsOfExperience}, 0)
+        let averageExperience = totalExperience / users.length;
         console.log(totalExperience);
+        console.log(averageExperience);
 
     //q.5
     // TODO: Use .reduce to get the longest email from the list of users.
-        let longestEmail = emails.reduce((longest, email) => {
-
-        }, 0);
+        let longestEmail = emails.reduce((acc, email) => {
+            if (acc.length > email.length) {
+                return acc;
+            }
+            return email;
+        }, "");
         console.log(longestEmail);
 
     //q.6
     // TODO: Use .reduce to get the list of user's names in a single string. Example: Your instructors are: ryan, luis, zach, fernando, justin.
-        let userList = users.reduce((name, user) => {
-
-        }, 0);
+        let userList = users.reduce((name, user, index) => {
+            if (index === 0){
+                return user.name;
+            } return name += ", " + user.name;
+        }, "");
         console.log(userList);
+
+    //BONUS
+    // TODO: Use .reduce to get the unique list of languages from the list of users.
+        let langArrays = users.map((user)=> {return user.languages});
+        console.log(langArrays);
+
+        let flattenLang = langArrays.reduce((acc, lang)=>{
+            return acc.concat(lang);
+        },[]);
+
+        console.log(flattenLang);
+
+        let listedLang = flattenLang.reduce((acc, val) =>{
+           if (acc.indexOf(val) === -1){
+               acc.push(val)
+           }
+           return acc
+        }, []);
+
+        console.log(listedLang);
 
 })();
