b9fad7f9ee2f91f35b43d573dac6367585b640f4
Chelsea <chelsea.e.rolland@gmail.com>
2020-09-21 16:18:34 -0500

    completed the promises exercises

--- js/promises.js
+++ js/promises.js
@@ -1,31 +1,63 @@
 (function(){
     "use strict";
 
-    // let gitRepoUrl = "https://api.github.com/users/chelsearolland/received_events";
-    // let gitRepoUrl = "https://api.github.com/repos/chelsearolland/codeup-web-exercises/commits";
-    let gitRepoUrl = "https://api.github.com/users/chelsearolland/repos"; // WORKS
-    // let gitRepoUrl = "https://api.github.com/users/chelsearolland/repos/search/commits"; DOESNT WORK
-    // let gitRepoUrl = "https://api.github.com/users/chelsearolland/search/commits"; DOESNT WORK
-    // let gitRepoUrl = "https://api.github.com/search/commits"; DOESNT WORK
-    // let gitRepoUrl = "https://api.github.com/repos/chelsearolland/codeup-web-exercises/commits";
-    // let gitRepoUrl = "https://api.github.com/users/chelsearolland/received_events";
-    // let gitRepoUrl = "https://api.github.com/users/chelsearolland/received_events";
-
-    // /repos/:owner/:repo/commits
-
-    fetch(gitRepoUrl, {headers: {'Authorization': gitRepoToken}})
-        .then(response => response.json())
-        .then(data => {
-            console.log(data);
-            data.forEach(info => {
-                console.log(info.name)
-            });
-        })
-        .catch(error => console.log(error));
-
-
     function githubPromise (username) {
-        // let githubRepoURL = `https://api.github.com/users/${username}/repos`;
+        let githubRepoURL = `https://api.github.com/users/${username}/events/public`;
+        fetch(githubRepoURL, {headers: {'Authorization': gitRepoToken}})
+            .then(response => response.json())
+            .then(data => {
+                // console.log(data);
+                let html = "";
+                let commitArray = [];
+                $("#commit").empty();
+
+                data.forEach(info => {
+                    if (info.type === "PushEvent") {
+                        commitArray.push(info)
+                    }
+                });
+                    console.log(commitArray);
+
+                    let repository = commitArray[0].repo.name;
+                    let username = commitArray[0].actor.login;
+                    let time = new Date (commitArray[0].created_at);
+                    let recentCommitMessage = commitArray[0].payload.commits[0].message
+
+                    html += `<p><strong>User:</strong> ${username}</p>` +
+                        `<p><strong>Repository:</strong> ${repository}</p>` +
+                        `<p><strong>Most Recent Commit:</strong> ${recentCommitMessage}</p>` +
+                        `<p><strong>Time:</strong> ${time}</p>`
+
+                    $("#commit").append(html);
+
+            })
+                    .catch(error => console.log(error));
+
     }
+
+    $("#user").click(function(e){
+        e.preventDefault();
+        let userInput = $("#userInput").val().trim();
+        if (userInput !== ""){
+            githubPromise(userInput);
+            $("#userInput").val("");
+        }
+    });
+
+    $("#userInput").keypress(function(e){
+        if (e.keyCode === 13) {
+            e.preventDefault();
+            let userInput = $("#userInput").val().trim();
+            if (userInput !== "") {
+                githubPromise(userInput);
+                $("#userInput").val("");
+            }
+        }
+    });
+
+
+    // document.querySelector("#user").addEventListener("click", githubPromise)
+
+    // githubPromise("chelsearolland");
 
 })();
